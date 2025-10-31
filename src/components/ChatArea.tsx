19607b0dc1498212ba3188933b58cd2a134be6fa
akasheyy <vadakkangaraakash@gmail.com>
2025-10-31 15:39:41 +0530

    Version 1.2.1.2: Fix chat messages to show oldest first (chronological order)

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -59,7 +59,7 @@ const ChatArea = ({ currentUserId, selectedUserId }: ChatAreaProps) => {
         .or(
           `and(sender_id.eq.${currentUserId},receiver_id.eq.${selectedUserId}),and(sender_id.eq.${selectedUserId},receiver_id.eq.${currentUserId})`
         )
-        .order("created_at", { ascending: false });
+        .order("created_at", { ascending: true });
 
       if (!error && data) {
         setMessages(data);
